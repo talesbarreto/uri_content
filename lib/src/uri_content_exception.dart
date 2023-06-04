cc35bc44d522fa80b4880e3a6fc5c2a9b37525e5
Barreto <barreto.tales@gmail.com>
2023-06-04 11:35:13 -0300

    test for content uri scheme

---
+++ lib/src/uri_content_exception.dart
@@ -0,0 +1,20 @@
+class UriContentError implements Exception {
+  final String error;
+
+  const UriContentError(this.error);
+
+  static const dataSchemeWithNoData =
+      UriContentError("The URI has a data scheme, but its data is null.");
+
+  static const contentOnlySupportedByAndroid =
+      UriContentError("`content` scheme is only supported on Android.");
+}
+
+class UnsupportedSchemeError implements UriContentError {
+  @override
+  final String error;
+
+  UnsupportedSchemeError(String scheme)
+      : error = "scheme `$scheme` is not supported. "
+            "Feel free to open an issue or submit an pull request at https://github.com/talesbarreto/uri_content";
+}
